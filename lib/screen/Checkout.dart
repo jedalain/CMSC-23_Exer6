c7fa5ef85b0852f4686e2a3683c53bd4e31ebc87
hey-ey-el <110464712+jedalain@users.noreply.github.com>
2024-04-13 07:58:03 +0800

    initialized checkout screen

---
+++ lib/screen/Checkout.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/screen/MyCart.dart';
+
+class Checkout extends StatefulWidget {
+  Checkout({super.key});
+
+  @override
+  State<Checkout> createState() => _CheckoutState();
+}
+
+class _CheckoutState extends State<MyCatalog> {
+  
+}
