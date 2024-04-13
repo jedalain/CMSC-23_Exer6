cde5cf05fa159db7d57c5389ee5cf2198c519570
hey-ey-el <110464712+jedalain@users.noreply.github.com>
2024-04-13 09:05:49 +0800

    fix: added itemCount for list builder

--- lib/screen/MyCart.dart
+++ lib/screen/MyCart.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/model/Item.dart';
 import 'package:my_app/provider/shoppingcart_provider.dart';
+import 'package:my_app/screen/Checkout.dart';
 import 'package:provider/provider.dart';
 
-class MyCatalog extends StatefulWidget {
-  const MyCatalog({super.key});
+class MyCart extends StatefulWidget {
+  const MyCart({super.key});
 
   @override
-  State<MyCatalog> createState() => _MyCartState();
+  State<MyCart> createState() => _MyCartState();
 }
 
-class _MyCartState extends State<MyCatalog> {
+class _MyCartState extends State<MyCart> {
   @override
   Widget build(BuildContext context) {
     // get items added by user in the shopping cart
@@ -22,6 +23,8 @@ class _MyCartState extends State<MyCatalog> {
       body: productsInCart.isNotEmpty ? 
         // shopping cart has content/s
         ListView.builder(
+          itemCount: productsInCart.length, // get number of products in cart
+
           itemBuilder: (BuildContext context, int index) {
             var productName = productsInCart[index].name;
             var productPrice = productsInCart[index].price;
@@ -39,6 +42,10 @@ class _MyCartState extends State<MyCatalog> {
                     .read<ShoppingCart>()
                     .removeItem(productName);
                   
+                  setState(() {
+                      context.read<ShoppingCart>().removeItem(productName);
+                    });
+                    
                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                     content: Text("$productName was removed from you cart."),
                     duration: const Duration(seconds: 1, milliseconds: 100),
@@ -51,14 +58,54 @@ class _MyCartState extends State<MyCatalog> {
         )
         :
         // shopping cart is empty
-        const Text("Shopping cart is empty."),
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Shopping cart is empty."),
+              TextButton(
+                onPressed: () { Navigator.pop(context); }, 
+                child: const Text("Go to catalog")
+              )
+            ],
+          ),
+        ), 
 
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.storefront),
+        floatingActionButton: 
+        // checkout button
+        FloatingActionButton(
+          child: const Icon(Icons.shopping_cart_checkout),
+
           onPressed: () {
-            Navigator.pushNamed(context, "/catalog");
+            productsInCart.isNotEmpty ?
+              // go to checkout
+              showDialog(
+                context: context, builder: (BuildContext context) {
+                  return Checkout();
+                }
+              )
+              // Navigator.pushNamed(context, "/checkout")
+            :
+              // show warning message
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: const Text("Add products to proceed to checkout"),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  );
+                },
+              );
           },
-        ),
+        )
     );
   }
 }
