0e5ed7912c951e4b93dedffccf3bbdf9970c2963
hey-ey-el <110464712+jedalain@users.noreply.github.com>
2024-04-13 09:05:32 +0800

    feat: made total modal for checkout

--- lib/screen/Checkout.dart
+++ lib/screen/Checkout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/screen/MyCart.dart';
+import 'package:my_app/provider/shoppingcart_provider.dart';
+import 'package:provider/provider.dart';
 
 class Checkout extends StatefulWidget {
   Checkout({super.key});
@@ -8,6 +9,33 @@ class Checkout extends StatefulWidget {
   State<Checkout> createState() => _CheckoutState();
 }
 
-class _CheckoutState extends State<MyCatalog> {
-  
+class _CheckoutState extends State<Checkout> {
+  @override
+  Widget build(BuildContext context) {
+    // get items and total price
+    double cartTotal = context.watch<ShoppingCart>().cartTotal;
+    
+    return AlertDialog(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30), // Add some padding
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Your total is: $cartTotal"),
+            const Text("Thank you for shopping with us!")
+          ],
+        ),
+
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              context.read<ShoppingCart>().removeAll(); // empty cart
+              Navigator.of(context).pop(); // close dialog box
+              Navigator.of(context).pop(); // go back to catalog
+            },
+            child: const Text('HOME'),
+          ),
+        ],
+    );
+  }
 }
