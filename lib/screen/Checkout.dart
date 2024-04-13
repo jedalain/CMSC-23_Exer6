873b0283c88b0ca56f8d7ee65c1e2ae3a98a6a3a
hey-ey-el <110464712+jedalain@users.noreply.github.com>
2024-04-13 09:45:09 +0800

    feat: added confirm button and item list for checkout

--- lib/screen/Checkout.dart
+++ lib/screen/Checkout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/model/Item.dart';
 import 'package:my_app/provider/shoppingcart_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -13,35 +14,69 @@ class _CheckoutState extends State<Checkout> {
   @override
   Widget build(BuildContext context) {
     // get items and total price
+    List<Item> productsInCart = context.watch<ShoppingCart>().cart;
     double cartTotal = context.watch<ShoppingCart>().cartTotal;
     
-    return AlertDialog(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30), // Add some padding
-        content: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              "Your total is: $cartTotal",
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600
-              ),
-            ),
-            const Text("Thank you for shopping with us!")
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Checkout"),
+      ),
 
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(); // close dialog box
-              Navigator.of(context).pop(); // go back to catalog
-              context.read<ShoppingCart>().removeAll(); // empty cart
-            },
-            child: const Text('HOME'),
-          ),
-        ],
+      body: Padding(
+          padding: const EdgeInsets.only(bottom: 30.0),
+          child: Column(
+            children: [
+              Flexible(
+                child: ListView.builder(
+                  itemCount: productsInCart.length, // get number of products in cart
+
+                  itemBuilder: (BuildContext context, int index) {
+                    var productName = productsInCart[index].name;
+                    var productPrice = productsInCart[index].price;
+
+                    return ListTile(
+                      leading: const Icon(Icons.star),
+                      
+                      title: Text(productName),
+
+                      trailing: Text("$productPrice")
+                    );
+                  }
+                ) 
+              ),
+
+              Text(
+                "Your total is: $cartTotal",
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600
+                ),
+              ),
+              
+              // checkout button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(); // close dialog box
+                      Navigator.of(context).pop(); // go back to catalog
+                      context.read<ShoppingCart>().removeAll(); // empty cart
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Payment successful."),
+                          duration: Duration(seconds: 1, milliseconds: 100),
+                        ),
+                      );
+                    },
+                    
+                    child: const Text("Confirm")
+                  )
+                ],
+              ),
+            ],
+          )
+        )
     );
   }
 }
